b0ddd739fcbf808602b88d5789b3d99c2399b7e6
igorfcouto <79174143+igorfcouto@users.noreply.github.com>
2024-09-20 15:06:09 -0400

    Add files via upload

---
+++ BadHabitApp/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace BadHabitApp.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Habit> Habits { get; set; }
+    }
+}
