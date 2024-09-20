d8003499200acf9563e5315d2b9bc36c93a50c04
igorfcouto <79174143+igorfcouto@users.noreply.github.com>
2024-09-20 15:01:48 -0400

    Add files via upload

---
+++ BadHabitApp/Models/HabitLog.cs
@@ -0,0 +1,12 @@
+namespace BadHabitApp.Models
+{
+    public class HabitLog
+    {
+        public int Id { get; set; }
+        public DateTime DateLogged { get; set; }
+        public bool Completed { get; set; }
+        public string Notes { get; set; }
+        public int HabitId { get; set; }
+        public Habit Habit { get; set; }
+    }
+}
