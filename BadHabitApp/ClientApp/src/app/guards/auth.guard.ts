5890e1f10cf5f02eafaba22aa9034be827f41f97
vulcandth <vulcandth@gmail.com>
2024-09-21 18:27:30 -0500

    Upgrade to angular 16

--- BadHabitApp/ClientApp/src/app/guards/auth.guard.ts
+++ BadHabitApp/ClientApp/src/app/guards/auth.guard.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   constructor(private authService: AuthService, private router: Router) { }
 
   canActivate(): boolean {
