b3ddabd70d1c51aebbdc88825df721c416a383d5
Jamal Bryan <jamal_bryan22@yahoo.com>
2024-11-08 03:53:12 -0500

    Remove default app components

--- BadHabitApp/ClientApp/src/app/app.module.ts
+++ BadHabitApp/ClientApp/src/app/app.module.ts
@@ -8,8 +8,6 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HabitsComponent } from './habits/habits.component';
@@ -25,8 +23,6 @@ import { JwtHelperService, JWT_OPTIONS } from '@auth0/angular-jwt';
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     LoginComponent,
     RegisterComponent,
     HabitsComponent
@@ -40,8 +36,6 @@ import { JwtHelperService, JWT_OPTIONS } from '@auth0/angular-jwt';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'register', component: RegisterComponent },
       { path: 'login', component: LoginComponent },
-      { path: 'counter', component: CounterComponent, canActivate: [AuthGuard] },
-      { path: 'fetch-data', component: FetchDataComponent },
       { path: 'habits', component: HabitsComponent, canActivate: [AuthGuard] }
     ])
   ],
